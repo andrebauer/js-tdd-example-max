d1b0af9832b3d54b93e2821bb082cedaeb0c7ce1
André Bauer <a.bauer@ovm-kassel.de>
2018-11-10 14:50:56 +0100

    Add a (failing) spec.

---
+++ spec/example.spec.js
@@ -0,0 +1,13 @@
+describe('example', () => {
+    const example = require('../lib/example')
+
+    describe('add', () => {
+        it('should return the sum of two given numbers.', () => {
+            expect(example.add(3, 4)).toEqual(7)
+            expect(example.add(-2, 5)).toEqual(3)
+            expect(example.add(-4, -19)).toEqual(-23)
+            expect(example.add(0, 4)).toEqual(4)
+            expect(example.add(-5, 0)).toEqual(-5)
+        })
+    })
+})
